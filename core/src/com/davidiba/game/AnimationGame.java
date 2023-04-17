c90bb192a8dac4648afdf942123d29827f9de1b1
DavidIbanezMoya <davidibanezmoya@gmail.com>
2023-04-17 16:01:28 +0200

    Initial commit

---
+++ core/src/com/davidiba/game/AnimationGame.java
@@ -0,0 +1,38 @@
+package com.davidiba.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class AnimationGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	public Animation<TextureRegion> runningAnimation;
+
+//...
+
+	runningAnimation = new Animation<TextureRegion>(0.033f, atlas.findRegions("running"), Animation.PlayMode.LOOP);
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
