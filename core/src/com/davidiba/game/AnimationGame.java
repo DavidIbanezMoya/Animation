085c1fd3812ee7cf6ba5d2f72047bd1379ff8b91
DavidIbanezMoya <davidibanezmoya@gmail.com>
2023-04-17 18:48:04 +0200

    Frames

--- core/src/com/davidiba/game/AnimationGame.java
+++ core/src/com/davidiba/game/AnimationGame.java
@@ -1,6 +1,7 @@
 package com.davidiba.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,23 +12,59 @@ public class AnimationGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	public Animation<TextureRegion> runningAnimation;
+	TextureRegion frames[] = new TextureRegion[16];
+	Animation<TextureRegion> gold;
 
 //...
 
-	runningAnimation = new Animation<TextureRegion>(0.033f, atlas.findRegions("running"), Animation.PlayMode.LOOP);
+	//runningAnimation = new Animation<TextureRegion>(0.033f, atlas.findRegions("running"), Animation.PlayMode.LOOP);
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("goldSprite.jpg");
+		//img = new Texture("sprite-animation4.jpg");
+
+		// per cada frame cal indicar x,y,amplada,alçada
+
+		//Sprite de la PT
+		frames[0] = new TextureRegion(img,0,0,64,64);
+		frames[1] = new TextureRegion(img,0,64,64,64);
+		frames[2] = new TextureRegion(img,0,128,64,64);
+		frames[3] = new TextureRegion(img,0,192,64,64);
+		frames[4] = new TextureRegion(img,64,0,64,64);
+		frames[5] = new TextureRegion(img,64,64,64,64);
+		frames[6] = new TextureRegion(img,64,128,64,64);
+		frames[7] = new TextureRegion(img,64,192,64,64);
+		frames[8] = new TextureRegion(img,128,0,64,64);
+		frames[9] = new TextureRegion(img,128,64,64,64);
+		frames[10] = new TextureRegion(img,128,128,64,64);
+		frames[11] = new TextureRegion(img,128,192,64,64);
+		frames[12] = new TextureRegion(img,192,0,64,64);
+		frames[13] = new TextureRegion(img,192,64,64,64);
+		frames[14] = new TextureRegion(img,192,128,64,64);
+		frames[15] = new TextureRegion(img,192,192,122,109);
+
+		gold = new Animation<TextureRegion>(0.25f,frames);
+
+		/*
+		frames[0] = new TextureRegion(img,9,190,107,112);
+		frames[1] = new TextureRegion(img,124,190,113,107);
+		frames[2] = new TextureRegion(img,248,190,101,110);
+		frames[3] = new TextureRegion(img,364,190,122,109);*/
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		float stateTime = Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+		TextureRegion frame = gold.getKeyFrame(stateTime,true);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		batch.draw(frame, 200, 100);
+// si volem invertir el sentit, ho podem fer amb el paràmetre scaleX=-1
+		batch.draw(frame, 200, 100, 0, 0,
+				frame.getRegionWidth(),frame.getRegionHeight(),-1,1,0);batch.end();
 	}
 	
 	@Override
