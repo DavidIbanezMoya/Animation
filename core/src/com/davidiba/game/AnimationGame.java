e80c380976d83b8075aa176a54a4191cfd7843b3
DavidIbanezMoya <davidibanezmoya@gmail.com>
2023-04-18 17:02:21 +0200

    Animacio i creant les direccions

--- core/src/com/davidiba/game/AnimationGame.java
+++ core/src/com/davidiba/game/AnimationGame.java
@@ -14,6 +14,7 @@ public class AnimationGame extends ApplicationAdapter {
 	public Animation<TextureRegion> runningAnimation;
 	TextureRegion frames[] = new TextureRegion[16];
 	Animation<TextureRegion> gold;
+	Float stateTime = 0.0f;
 
 //...
 
@@ -45,26 +46,41 @@ public class AnimationGame extends ApplicationAdapter {
 		frames[14] = new TextureRegion(img,192,128,64,64);
 		frames[15] = new TextureRegion(img,192,192,122,109);
 
+		//Posar les direccions segons la textureRegion
+		frames[0] = new TextureRegion(img,0,0,64,64);
+		frames[1] = new TextureRegion(img,0,64,64,64);
+		frames[2] = new TextureRegion(img,0,128,64,64);
+		frames[3] = new TextureRegion(img,0,192,64,64);
+
+		frames[4] = new TextureRegion(img,64,0,64,64);
+		frames[5] = new TextureRegion(img,64,64,64,64);
+		frames[6] = new TextureRegion(img,64,128,64,64);
+		frames[7] = new TextureRegion(img,64,192,64,64);
+
+		frames[8] = new TextureRegion(img,128,0,64,64);
+		frames[9] = new TextureRegion(img,128,64,64,64);
+		frames[10] = new TextureRegion(img,128,128,64,64);
+		frames[11] = new TextureRegion(img,128,192,64,64);
+
+		frames[12] = new TextureRegion(img,192,0,64,64);
+		frames[13] = new TextureRegion(img,192,64,64,64);
+		frames[14] = new TextureRegion(img,192,128,64,64);
+		frames[15] = new TextureRegion(img,192,192,122,109);
 		gold = new Animation<TextureRegion>(0.25f,frames);
 
-		/*
-		frames[0] = new TextureRegion(img,9,190,107,112);
-		frames[1] = new TextureRegion(img,124,190,113,107);
-		frames[2] = new TextureRegion(img,248,190,101,110);
-		frames[3] = new TextureRegion(img,364,190,122,109);*/
+		//Implementar el panel
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		float stateTime = Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+		//ScreenUtils.clear(1, 0, 0, 1);
+		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 		TextureRegion frame = gold.getKeyFrame(stateTime,true);
 
 		batch.begin();
-		batch.draw(frame, 200, 100);
 // si volem invertir el sentit, ho podem fer amb el paràmetre scaleX=-1
 		batch.draw(frame, 200, 100, 0, 0,
-				frame.getRegionWidth(),frame.getRegionHeight(),-1,1,0);batch.end();
+				frame.getRegionWidth(),frame.getRegionHeight(),3,3,0);batch.end();
 	}
 	
 	@Override
